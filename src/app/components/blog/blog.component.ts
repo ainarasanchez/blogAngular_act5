81d5a060bceae27c09c9a0df2a9c597f74bf90ea
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-12 21:38:50 +0100

    El componente blog se queda como un contenedor
    No tiene otra función

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,60 +1,17 @@
 import { Component } from '@angular/core';
-import { IPost } from '../../interfaces/ipost.interface';
-import { DatePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormComponent } from "../form/form.component";
+import { PostListComponent } from "../post-list/post-list.component";
 
 @Component({
   selector: 'app-blog',
-  imports: [DatePipe, FormsModule],
+  imports: [FormComponent, PostListComponent],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-  newPost: IPost = { id: 0, title: '', url: '', author: '', date: '', body: '' };
-  id: number = 1;
-  arrPosts: IPost[] = [
-    {
-        id: 1,
-        title: 'Primera Publicación',
-        url: 'https://www.costacruceros.es/content/dam/costa/costa-magazine/articles-magazine/travel/crete-travel/creta_m.jpg.image.694.390.low.jpg',
-        author: 'Autor Uno',
-        date: '2025-03-12',
-        body: 'Este es el contenido de la <strong>primera publicación.<strong/>'
-    },
-    {
-        id: 2,
-        title: 'Segunda Publicación',
-        url: 'https://www.costacruceros.es/content/dam/costa/costa-magazine/articles-magazine/travel/crete-travel/creta_m.jpg.image.694.390.low.jpg',
-        author: 'Autor Dos',
-        date: '2025-03-11',
-        body: 'Este es el contenido de la segunda publicación.'
-    },
-    {
-        id: 3,
-        title: 'Tercera Publicación',
-        url: 'https://www.costacruceros.es/content/dam/costa/costa-magazine/articles-magazine/travel/crete-travel/creta_m.jpg.image.694.390.low.jpg',
-        author: 'Autor Tres',
-        date: '2025-03-10',
-        body: 'Este es el contenido de la tercera publicación.'
-    }
-  ];
-
   ngOnInit() {
     //this.getData();
-
     }
-  
-  guardarPost() {
-    if(this.newPost.title !== "" && this.newPost.author !== "" && this.newPost.date !== "" && this.newPost.body !== "" && this.newPost.url !== "") {
-      // puedo insertar el elemento en el array
-      this.newPost.id = this.id;
-      this.arrPosts.push(this.newPost);
-      this.id++;
-      this.newPost = { id: 0, title: '', url: '', author: '', date: '', body: '' };
-    } else {
-      alert('Todos los campos son obligatorios');
-    }
-  }
 
   // getData(): string {
   //     let html = '';
@@ -73,4 +30,4 @@ export class BlogComponent {
   //     })
   //     return html;
   // }
-  }
+}
