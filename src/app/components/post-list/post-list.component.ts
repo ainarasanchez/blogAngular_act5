d90c2726e63899dc330d60fea6f49d07718d5be1
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-12 21:42:47 +0100

    Inyecto el servicio al arrPosts

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { DatePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IPost } from '../../interfaces/ipost.interface';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -8,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent {
+  arrPosts: IPost[] = [];
+
+  // le pedimos al servicio que nos de los datos del post
+  postService = inject(PostsService);
+
+  ngOnInit() {
+    this.arrPosts = this.postService.getAll();
+  }
 
 }
