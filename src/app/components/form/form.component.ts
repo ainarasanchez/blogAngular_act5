d2676f227931945c4f2deb76fa576631eb3b1d35
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-12 21:53:31 +0100

    Inyecto el servicio en guardarPost

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IPost } from '../../interfaces/ipost.interface';
 import { FormsModule } from '@angular/forms';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-form',
@@ -11,12 +12,19 @@ import { FormsModule } from '@angular/forms';
 export class FormComponent {
     newPost: IPost = { id: 0, title: '', url: '', author: '', date: '', body: '' };
 
+    postsService = inject(PostsService);
+
     guardarPost() {
       if(this.newPost.title !== "" && this.newPost.author !== "" && this.newPost.date !== "" && this.newPost.body !== "" && this.newPost.url !== "") {
-        // puedo insertar el elemento en el array
+        //puedo insertar el elemento en el array
         //this.newPost.id = this.id;
         //this.arrPosts.push(this.newPost);
         //this.id++;
+
+        //llevar el newPost al servicio, para ello tenemos que inyectar el servicio dentro de este componente. 
+        let response = this.postsService.insert(this.newPost);
+        alert(response);
+
         this.newPost = { id: 0, title: '', url: '', author: '', date: '', body: '' };
       } else {
         alert('Todos los campos son obligatorios');
