84314f9dbb1b7e697940fafb7eefad47fbfcf10f
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-12 21:28:08 +0100

    Creación y maquetación componente form

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IPost } from '../../interfaces/ipost.interface';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  imports: [FormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+    newPost: IPost = { id: 0, title: '', url: '', author: '', date: '', body: '' };
+    id: number = 1;
+
+    guardarPost() {
+      if(this.newPost.title !== "" && this.newPost.author !== "" && this.newPost.date !== "" && this.newPost.body !== "" && this.newPost.url !== "") {
+        // puedo insertar el elemento en el array
+        this.newPost.id = this.id;
+        //this.arrPosts.push(this.newPost);
+        this.id++;
+        this.newPost = { id: 0, title: '', url: '', author: '', date: '', body: '' };
+      } else {
+        alert('Todos los campos son obligatorios');
+      }
+    }
+
+}
