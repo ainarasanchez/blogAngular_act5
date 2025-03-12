b915770b13684ec373215dc3be7acce373aeaac6
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-12 22:17:21 +0100

    Utilizamos @Input para acabar la card

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IPost } from '../../interfaces/ipost.interface';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-post-card',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './post-card.component.html',
   styleUrl: './post-card.component.css'
 })
 export class PostCardComponent {
-
+  @Input() miPost!: IPost;
 }
